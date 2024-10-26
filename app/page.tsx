7b6e9b8f46f1b350c9275c13fe0c5d9f6be0699a
immeo <wayfarer245@gmail.com>
2024-10-26 19:31:16 +0300

    04--add cdolors and component Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Button from '@/components/Button/Button';
 import Htag from '@/components/Htag/Htag';
 
 export default function Home() {
 	return (
 		<div>
 			<Htag tag='h1'>Nекст</Htag>
+			<Button appearance='primary'>primary</Button>
+			<Button appearance='ghost'>ghost</Button>
 		</div>
 	);
 }
